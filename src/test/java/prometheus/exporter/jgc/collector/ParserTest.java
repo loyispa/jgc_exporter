f64c93eb771ddb617093e7f3196fb5a3aebe00d6
loyispa <litiegen456250@gmail.com>
2023-12-10 17:56:50 +0800

    update test

--- src/test/java/prometheus/exporter/jgc/collector/ParserTest.java
+++ src/test/java/prometheus/exporter/jgc/collector/ParserTest.java
@@ -15,35 +15,51 @@
  */
 package prometheus.exporter.jgc.collector;
 
+import com.microsoft.gctoolkit.event.GarbageCollectionTypes;
+import com.microsoft.gctoolkit.event.g1gc.G1Mixed;
+import com.microsoft.gctoolkit.event.generational.GenerationalGCPauseEvent;
+import com.microsoft.gctoolkit.event.zgc.ZGCCycle;
+import com.microsoft.gctoolkit.message.ChannelName;
+import java.io.File;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import prometheus.exporter.jgc.collector.parser.GCAggregator;
+
 public class ParserTest {
-    //
-    //    @Before
-    //    public void init() {
-    //        MockitoAnnotations.openMocks(this);
-    //    }
-    //
-    //    @Test
-    //    public void test1() throws Exception {
-    //        File log = new File("src/test/resources/parser/g1.log");
-    //        GCAggregator aggregator = Mockito.spy(new GCAggregator(log));
-    //        Files.lines(log.toPath()).forEach(aggregator::receive);
-    //        Mockito.verify(aggregator, Mockito.times(1)).recordG1GCEvent(Mockito.notNull());
-    //    }
-    //
-    //    @Test
-    //    public void test2() throws Exception {
-    //        File log = new File("src/test/resources/parser/zgc.log");
-    //        GCAggregator aggregator = Mockito.spy(new GCAggregator(log));
-    //        Files.lines(log.toPath()).forEach(aggregator::receive);
-    //        Mockito.verify(aggregator, Mockito.times(1)).recordZGCEvent(Mockito.isNotNull());
-    //    }
-    //
-    //    @Test
-    //    public void test3() throws Exception {
-    //        File log = new File("src/test/resources/parser/cms.log");
-    //        GCAggregator aggregator = Mockito.spy(new GCAggregator(log));
-    //        Files.lines(log.toPath()).forEach(aggregator::receive);
-    //        Mockito.verify(aggregator, Mockito.times(20))
-    //                .recordGenerationalGCEvent(Mockito.isNotNull());
-    //    }
+
+    @Before
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void test1() throws Exception {
+        File log = new File("src/test/resources/parser/g1.log");
+        GCAggregator aggregator = Mockito.spy(new GCAggregator(log));
+        G1Mixed event = Mockito.mock(G1Mixed.class);
+        aggregator.publish(ChannelName.G1GC_PARSER_OUTBOX, event);
+        Mockito.verify(aggregator, Mockito.times(1)).recordG1GCEvent(event);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        File log = new File("src/test/resources/parser/zgc.log");
+        GCAggregator aggregator = Mockito.spy(new GCAggregator(log));
+        ZGCCycle event = Mockito.mock(ZGCCycle.class);
+        aggregator.publish(ChannelName.ZGC_PARSER_OUTBOX, event);
+        Mockito.verify(aggregator, Mockito.times(1)).recordZGCEvent(event);
+    }
+
+    @Test
+    public void test3() throws Exception {
+        File log = new File("src/test/resources/parser/cms.log");
+        GCAggregator aggregator = Mockito.spy(new GCAggregator(log));
+        GenerationalGCPauseEvent event = Mockito.mock(GenerationalGCPauseEvent.class);
+        Mockito.when(event.getGarbageCollectionType())
+                .thenReturn(GarbageCollectionTypes.CMSPausePhase);
+        aggregator.publish(ChannelName.GENERATIONAL_HEAP_PARSER_OUTBOX, event);
+        Mockito.verify(aggregator, Mockito.times(1)).recordGenerationalGCEvent(event);
+    }
 }
