d2d502bb0e0f3311ed135bf6f9e35f2494bdbd10
loyispa <litiegen456250@gmail.com>
2024-02-05 11:22:08 +0800

    harden native build and logging

--- bin/run.sh
+++ bin/run.sh
@@ -2,18 +2,18 @@
 
 BASE_DIR=$(dirname $0)/..
 
-EXPORTER_BASE_DIR="${BASE_DIR}"
+export JGC_EXPORTER_BASE_DIR="${BASE_DIR}"
 
 NATIVE_MODE="false"
 
 # prefer native executable
-if test -e "${BASE_DIR}"/lib/jgc_exporter; then
+if test -e "${BASE_DIR}"/native/jgc_exporter; then
   NATIVE_MODE="true"
 fi
 
-JGC_HEAP_OPTS="-Xmx256m"
-
-JGC_NATIVE_HEAP_OPTS="-Xmx64m"
+if [ "$1" == "--jar" ]; then
+    NATIVE_MODE="false"
+fi
 
 # Which java to use
 if [ -z "$JAVA_HOME" ]; then
@@ -22,8 +22,14 @@ else
   JAVA="$JAVA_HOME/bin/java"
 fi
 
+JGC_NATIVE_HEAP_OPTS="-Xmx64m"
+
+JGC_JAR_HEAP_OPTS="-Xmx256m"
+
 if [ "x$NATIVE_MODE" = "xtrue" ]; then
-  nohup "$BASE_DIR"/lib/jgc_exporter $JGC_NATIVE_HEAP_OPTS "$BASE_DIR"/conf/config.yaml > /dev/null 2>&1 &
+  echo "Using native executable: ${JGC_NATIVE_HEAP_OPTS}"
+  nohup "$BASE_DIR"/native/jgc_exporter $JGC_NATIVE_HEAP_OPTS "$BASE_DIR"/conf/config.yaml > /dev/null 2>&1 &
 else
-  nohup "$JAVA" $JGC_HEAP_OPTS -jar "$BASE_DIR"/lib/jgc_exporter.jar "$BASE_DIR"/conf/config.yaml > /dev/null 2>&1 &
+  echo "Using jar executable: ${JGC_JAR_HEAP_OPTS}"
+  nohup "$JAVA" $JGC_JAR_HEAP_OPTS -jar "$BASE_DIR"/lib/jgc_exporter.jar "$BASE_DIR"/conf/config.yaml > /dev/null 2>&1 &
 fi
